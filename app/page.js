5a2b6e7e5591dfa5fe7e9e7db338a78cba83a496
Shakeer G <gshakeer650@gmail.com>
2025-02-17 23:52:54 +0530

    Initial commit of my portfolio

---
+++ app/page.js
@@ -0,0 +1,42 @@
+'use client'
+import { useEffect, useState } from "react";
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
+import Services from "@/components/Services";
+import Work from "@/components/Work";
+import Footer from "@/components/Footer";
+export default function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(()=>{
+    if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia
+  ('(prefers-colors-scheme:dark)').matches) ){
+      setIsDarkMode(true)
+    }else{
+      setIsDarkMode(false)
+    }
+  },[])
+
+  useEffect(()=>{
+    if(isDarkMode){
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    }else{
+      document.documentElement.classList.remove('dark');
+      localStorage.theme= '';
+    }
+  },[isDarkMode])
+  return (
+    <>
+    <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode}/>
+    <Header isDarkMode={isDarkMode} />
+    <About isDarkMode={isDarkMode} />
+    <Services isDarkMode={isDarkMode} />
+    <Work isDarkMode={isDarkMode} />
+    <Contact isDarkMode={isDarkMode} />
+    <Footer isDarkMode={isDarkMode} />
+    </>
+  );
+}
